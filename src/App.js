bb1a761f4c45e37e5b464fce2df3555d127e7290
berniem <berniem@missionreadyhq.com>
2022-08-31 10:11:55 +1200

    add logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-// import logo from './turnerscars_logo.png';
+import logo from './turnerscars_logo.png';
   
 function App() {
   return (
     <div className='App'>
       
-      {/* <div className='logo'>
+      <div className='logo'>
         <img src={logo} className='logo' /> 
-      </div>   */}
+      </div>  
 
       <div className='search-bar'>
         <iframe style={{width:'100%', height:'1000px'}} src='https://ui.customsearch.ai/hosted-page?customconfig=44bae69b-5bf1-40e2-a4ac-b4380cb578e8&version=latest&market=en-US&q=' />
